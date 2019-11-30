84ca7cd438672a4fc91ad8992bcd26d00d774bd3
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-30 22:32:58 +0100

    open lab

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -6,7 +6,23 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+            string prefix = "";
+            string firstSection = "";
+            string secondSection = "";
+            for (int a = 0; a < 3; a++)
+            {
+                prefix += numbers[a];
+            }
+            for (int b = 3; b < 6; b++)
+            {
+                firstSection += numbers[b];
+            }
+            for (int c = 6; c < 10; c++)
+            {
+                secondSection += numbers[c];
+            }
+            return $"({prefix}) {firstSection}-{secondSection}";
         }
+
     }
 }
